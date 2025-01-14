dcc7ee98cbf16a3b6a746dcfac14c4c0ebac0821
Thomas Langui <thomas.langui971@gmail.comgit config --global user.email thomas.langui971@gmail.com>
2025-01-14 19:55:03 +0100

    fix(): resolution du problème avec la connexion admin.

--- Section Admin/Gestion Utilisateur/Gestion_Utilisateur.php
+++ Section Admin/Gestion Utilisateur/Gestion_Utilisateur.php
@@ -1,19 +1,18 @@
 <?php
 var_dump($_POST);
-$bddconnexion = new PDO('mysql:host=localhost;dbname=rmr_cinema;charset=utf8', 'root', '');
-$reqconnexion = $bddconnexion->prepare('SELECT * FROM utilisateur WHERE email = :email AND mdp = :mdp AND role = :role');
+$bddconnexion_admin = new PDO('mysql:host=localhost;dbname=rmr_cinema;charset=utf8', 'root', '');
+$reqconnexion = $bddconnexion_admin->prepare('SELECT * FROM utilisateur WHERE email = :email || mdp = :mdp AND role = :role');
 $reqconnexion->execute(array(
     'email' => $_POST['email'],
     'mdp' => $_POST['mdp'],
     'role' => $_POST['role']
 ));
 
-$donne = $reqconnexion->fetch();
-var_dump($donne);
-if ($donne == NULL) {
-    echo "vous n'avez pas de compte admin!";
+if ($_POST['role'] == NULL) {
+    echo "vous n'avez pas le role admin!";
 }
 else {
     session_start();
+    echo "vous êtes connecter en t'en qu'admin  ";
 }
 ?>
