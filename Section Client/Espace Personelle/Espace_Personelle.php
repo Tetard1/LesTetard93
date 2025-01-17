1b772e74d9a2e319ebc375c09bdc6b9522f630f2
Thomas Langui <t.langui@lprs.fr>
2025-01-17 09:12:28 +0100

    feat(): modification et ajout de fichier

--- Section Client/Espace Personelle/Espace_Personelle.php
+++ Section Client/Espace Personelle/Espace_Personelle.php
@@ -1,14 +1,21 @@
 <?php
 var_dump($_POST);
 $bdd = new PDO('mysql:host=localhost;dbname=tli3;charset=utf8', 'root', '');
-$req = $bdd->prepare('UPDATE inscrit SET mdp = :mdp WHERE email = :email');
+$req = $bdd->prepare('UPDATE inscrit SET mdp = :mdp WHERE email = :email AND mdp = :mdp');
 
 $req->execute(array(
-    'email' => $_POST['email'],
-    'mdp' => $_POST['mdp'],
+'email' => $_POST['email'],
+'mdp' => $_POST['mdp'],
 ));
 echo "Votre mot de passe a été changer avec succes ! ";
+var_dump($_POST);
+$bdd = new PDO('mysql:host=localhost;dbname=tli3;charset=utf8', 'root', '');
+$req = $bdd->prepare('DELETE FROM inscrit WHERE email = :email AND mdp = :mdp');
+$req->execute(array(
+    'email' => $_SESSION['email']
+));
 
+echo "Votre utilisateur a été suprimer avec succes ! ";
 
 var_dump($_POST);
 $bdd = new PDO('mysql:host=localhost;dbname=tli3;charset=utf8', 'root', '');
@@ -18,5 +25,4 @@ $req->execute(array(
     'email' => $_POST['email'],
 ));
 echo "Votre mot de passe a été changer avec succes ! ";
-
 
