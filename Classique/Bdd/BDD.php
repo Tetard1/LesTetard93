fd50a9d78ac708e93177b7382ee5755c4f5ad73d
Berynd <gabriel.berinde93@icloud.com>
2025-01-24 10:53:21 +0100

    feat: ajout classique

--- Classique/Bdd/BDD.php
+++ Classique/Bdd/BDD.php
@@ -7,7 +7,7 @@ class BDD
 
     public function __construct()
     {
-        $this->bdd = new PDO('mysql:host=localhost;dbname=rmr_cinema;charset=utf8', 'root', '');
+        $this->bdd = new PDO('mysql:host=localhost:8889;dbname=project;charset=utf8', 'root', 'root');
     }
 
     public function getBdd()
