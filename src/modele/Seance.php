ce70a187db8e5aef39a9c8fe7f46ffc0d1afa9e5
Ruth METAYER <METAYER_Ru@lprs.local>
2025-01-29 09:51:32 +0100

    Feat(): creation dd'une classe et de l'hydratation seance

--- src/modele/Seance.php
+++ src/modele/Seance.php
@@ -1,4 +1,101 @@
 <?php
 Class Seance{
-    private 
+    private $date;
+    private $heure;
+    private $nbPlcDispo;
+    private $titre;
+    private $nomSalle;
+
+
+    public function __construct(array $donnees){
+        $this->hydrate($donnees);
+
+    }
+    public function hydrate(array $donnees){
+        foreach($donnees as $key => $value){
+            $method = 'set'.ucfirst($key);
+            if(method_exists($this, $method)){
+                $this->$method($value);
+            }
+        }
+    }
+    /**
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param mixed $date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHeure()
+    {
+        return $this->heure;
+    }
+
+    /**
+     * @param mixed $heure
+     */
+    public function setHeure($heure)
+    {
+        $this->heure = $heure;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNbPlcDispo()
+    {
+        return $this->nbPlcDispo;
+    }
+
+    /**
+     * @param mixed $nbPlcDispo
+     */
+    public function setNbPlcDispo($nbPlcDispo)
+    {
+        $this->nbPlcDispo = $nbPlcDispo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * @param mixed $titre
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNomSalle()
+    {
+        return $this->nomSalle;
+    }
+
+    /**
+     * @param mixed $nomSalle
+     */
+    public function setNomSalle($nomSalle)
+    {
+        $this->nomSalle = $nomSalle;
+    }
 }
