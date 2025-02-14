512cf6b70067ed344d623b0ae38e4ee1f4c87332
Thomas Langui <thomas.langui971@gmail.com>
2025-02-14 09:42:12 +0100

    fix() changement d'une redirection de mon compte

--- src/repository/SalleRepo.php
+++ src/repository/SalleRepo.php
@@ -6,19 +6,19 @@ class SalleRepo{
         $this->bdd = new BDD();
     }
 
-    public function ajouterSalle(Salle $salle)  {
-        $sql="INSERT INTO salle(nom_salle,place_totale) VALUES(:nomSalle,:placeTotale)";
-        $req=$this->bdd->getBdd()->prepare($sql);
-        $res=$req->execute(array(
-            "nomSalle"=>$salle->getNomSalle(),
-            "placeTotale"=>$salle->getPlaceTotale()
-        ));
-        if($res){
-            return true;
-        }else {
-            return false;
+        public function ajouterSalle(Salle $salle)  {
+            $sql="INSERT INTO salle(nom_salle,place_totale) VALUES(:nomSalle,:placeTotale)";
+            $req=$this->bdd->getBdd()->prepare($sql);
+            $res=$req->execute(array(
+                "nomSalle"=>$salle->getNomSalle(),
+                "placeTotale"=>$salle->getPlaceTotale()
+            ));
+            if($res){
+                return true;
+            }else {
+                return false;
+            }
         }
-    }
 
         public function modificationSalle (Salle $salle)  {
 
@@ -51,15 +51,15 @@ class SalleRepo{
             $req->execute(
             );
             return $req->fetchall();
-    }
-    public function afficherLaSalle(Salle $salle){
-        $show="SELECT nom_salle,place_totale FROM `salle` WHERE id_salle=:idSalle";
-        $req=$this->bdd->getBdd()->prepare($show);
-        $req->execute([
+        }
+        public function afficherLaSalle(Salle $salle){
+            $show="SELECT nom_salle,place_totale FROM `salle` WHERE id_salle=:idSalle";
+            $req=$this->bdd->getBdd()->prepare($show);
+            $req->execute([
             'idSalle'=>$salle->getIdSalle()
         ]);
         return $req->fetch();
-    }
+        }
 
 
 }
