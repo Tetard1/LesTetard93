c02e8cfdc3221dba595a62e80072bf0f4368950a
Ruth METAYER <METAYER_Ru@lprs.local>
2025-03-05 10:08:20 +0100

    feat():Ajout d'un bouton de reservation

--- vue/filmDetail.php
+++ vue/filmDetail.php
@@ -69,6 +69,7 @@ $film = $liste->detailFilm($_GET['id']);
             <div class="btn-container">
                 <a href="filmAffiche.php"><button type='button' class='btn btn-primary'">Retour</button></a>
                 <a href="modifFilm.php?id=<?=$film->getId()?>"><button type='button' class='btn btn-warning'>Modifier</button></a>
+                <a href="ajoutReservation.php?id=<?=$film->getId()?>"><button type='button' class='btn btn-primary'>Reserver</button></a>
                <button type='button' class='btn btn-danger' onclick="document.getElementById('form_suppr').submit()">Suppprimer</button>
 
                 <form id="form_suppr" method="post" action="../src/traitement/traitementSuppressionFilm.php">
