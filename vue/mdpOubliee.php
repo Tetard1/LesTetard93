69b3ed3bf4ee158ba41c66385fa0a5e2165fbaab
Berynd <gabriel.berinde93@icloud.com>
2025-03-07 11:17:32 +0100

    feat(): thomas a ajouter le formulaire de mot de passe oublié

---
+++ vue/mdpOubliee.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Plus 2</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: #f4f4f4;
+        }
+        .container {
+            max-width: 400px;
+            margin-top: 50px;
+            background: white;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <h2 class="text-center">Mot de passe oublié</h2>
+    //mettre le chemin d'accèes pour le Form
+    <form method="POST" action="../src/#">
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control" id="email" name="email" placeholder="...@..." required>
+        </div>
+        <button type="submit" class="btn btn-primary w-100">Confirmer</button><br><br>
+    </form>
+    <a href="accueil.php"><button type="submit" class="btn btn-primary w-100">Acceuil</button></a><br><br>
+
+</div>
+</body>
+</html>
